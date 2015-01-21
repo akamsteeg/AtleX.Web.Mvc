c296c845656c3079c7a892a9ad6ec30bb4221c62
Alex Kamsteeg <alex@atlex.nl>
2015-01-21 23:04:49 +0100

    Initial commit

---
+++ src/AtleX.Web.Mvc/OutputCache/Providers/RedisOutputCacheProvider.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Caching;
+using StackExchange.Redis;
+
+namespace AtleX.Web.Mvc.OutputCache.Providers
+{
+    public class RedisOutputCacheProvider : OutputCacheProvider
+    {
+        public RedisOutputCacheProvider()
+        {
+        }
+
+        public override object Add(string key, object entry, DateTime utcExpiry)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override object Get(string key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Remove(string key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Set(string key, object entry, DateTime utcExpiry)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
