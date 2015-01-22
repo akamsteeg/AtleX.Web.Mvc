f146f9013e1f0c6a4abbff4c708d97f05b725f65
Alex Kamsteeg <alex@atlex.nl>
2015-01-22 20:41:50 +0100

    Updated documentation

--- src/AtleX.Web.Mvc/ControllerExtensions.cs
+++ src/AtleX.Web.Mvc/ControllerExtensions.cs
@@ -10,21 +10,45 @@ namespace AtleX.Web.Mvc
 {
     public static class ControllerExtensions
     {
+        /// <summary>
+        /// Does a HTTP/301 Redirect Permanently without
+        /// output HTML
+        /// </summary>
+        /// <param name="location">The URL to redirect to</param>
+        /// <returns></returns>
         public static System.Web.Mvc.ActionResult HttpRedirectPermanent(this Controller controller, string location)
         {
             return new PermanentRedirect(location);
         }
 
+        /// <summary>
+        /// Does a HTTP/301 Redirect Permanently without
+        /// output HTML
+        /// </summary>
+        /// <param name="location">The URL to redirect to</param>
+        /// <returns></returns>
         public static System.Web.Mvc.ActionResult HttpRedirectPermanent(this Controller controller, Uri location)
         {
             return controller.HttpRedirectPermanent(location);
         }
 
+        /// <summary>
+        /// Does a HTTP/302 Temporary Redirect without
+        /// output HTML
+        /// </summary>
+        /// <param name="location">The URL to redirect to</param>
+        /// <returns></returns>
         public static System.Web.Mvc.ActionResult HttpRedirectTemporary(this Controller controller, string location)
         {
             return new PermanentRedirect(location);
         }
 
+        /// <summary>
+        /// Does a HTTP/302 Temporary Redirect without
+        /// output HTML
+        /// </summary>
+        /// <param name="location">The URL to redirect to</param>
+        /// <returns></returns>
         public static System.Web.Mvc.ActionResult HttpRedirectTemporary(this Controller controller, Uri location)
         {
             return controller.HttpRedirectTemporary(location);
