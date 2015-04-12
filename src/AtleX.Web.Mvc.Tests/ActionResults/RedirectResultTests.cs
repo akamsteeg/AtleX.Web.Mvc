7621fc3b00a49f008076c51cb9c05a81407160d1
Alex Kamsteeg <alex@atlex.nl>
2015-04-12 19:48:00 +0200

    Added basic unit tests for redirect results

---
+++ src/AtleX.Web.Mvc.Tests/ActionResults/RedirectResultTests.cs
@@ -0,0 +1,47 @@
+﻿using AtleX.Web.Mvc.ActionResults;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace AtleX.Web.Mvc.Tests.ActionResults
+{
+    [TestFixture]
+    class RedirectResultTests
+    {
+        [Test]
+        public void PermanentRedirectSetsPermanentFlag_Successful()
+        {
+            PermanentRedirect r = new PermanentRedirect("http://localhost/");
+
+            Assert.IsTrue(r.Permanent);
+        }
+
+        [Test]
+        public void PermanentRedirectIsActionResult_Successful()
+        {
+            PermanentRedirect r = new PermanentRedirect("http://localhost/");
+
+            Assert.IsInstanceOf<ActionResult>(r);
+        }
+
+        [Test]
+        public void TemporaryRedirectDoesNotSetPermanentFlag_Successful()
+        {
+            TemporaryRedirect r = new TemporaryRedirect("http://localhost/");
+
+            Assert.IsFalse(r.Permanent);
+        }
+
+        [Test]
+        public void TemporaryRedirectIsActionResult_Successful()
+        {
+            TemporaryRedirect r = new TemporaryRedirect("http://localhost/");
+
+            Assert.IsInstanceOf<ActionResult>(r);
+        }
+    }
+}
